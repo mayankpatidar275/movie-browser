08401b7dbeda340f9c0f1968de8da0de786e4ddf
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-01-01 20:17:26 +0530

    fix: img optimization, jpg fallback

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -1,4 +1,6 @@
 import MovieImg from "../../assets/movies/movie-img.webp";
+// import MovieImgAvif from "../../assets/movies/movie-img.avif";
+import MovieImgJPG from "../../assets/movies/movie-img.jpg";
 
 function Carousel() {
   return (
@@ -11,12 +13,17 @@ function Carousel() {
         Main Hero Image
       </h2>
       <figure>
-        <img
-          className="w-full h-full object-cover object-bottom"
-          src={MovieImg}
-          alt="A visually captivating poster"
-          // loading="lazy"
-        />
+        <picture>
+          {/* WebP source for modern browsers */}
+          <source srcSet={MovieImg} type="image/webp" />
+          {/* Fallback image */}
+          <img
+            src={MovieImgJPG}
+            alt="A visually captivating poster"
+            className="w-full h-full object-cover object-bottom"
+            // loading="lazy"
+          />
+        </picture>
         <figcaption className="sr-only">
           A scenic poster representing the theme of the website.
         </figcaption>
