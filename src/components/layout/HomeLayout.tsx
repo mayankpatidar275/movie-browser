ea05790c4f41bc6d5dc5d5c1802531d913b86ea9
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-01-02 20:18:24 +0530

    fix: mobile navbar layout, navigate to home from logo

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -54,7 +54,15 @@ function HomeLayout({ children }: { children: React.ReactNode }) {
             <div className="relative flex items-center justify-between h-16 w-full">
               <div className="flex items-center justify-between w-full">
                 <div className="text-2xl font-bold text-gray-900 dark:text-primary flex gap-5 items-center">
-                  <span aria-label="Brand logo">MB</span>
+                  <span aria-label="Brand logo">
+                    <NavLink
+                      to={"/"}
+                      onClick={() => setOpen(false)}
+                      aria-label={`Navigate to home`}
+                    >
+                      MB
+                    </NavLink>
+                  </span>
                   <div className="hidden sm:block sm:ml-6">
                     <ul className="flex space-x-2">
                       {navItems.map((item, index) => (
@@ -100,8 +108,16 @@ function HomeLayout({ children }: { children: React.ReactNode }) {
               aria-expanded={open}
             >
               <div className="flex justify-between w-full h-16">
-                <div className="text-2xl font-bold text-gray-900 dark:text-primary flex w-full justify-end px-4">
-                  <span aria-label="Brand logo">MB</span>
+                <div className="text-2xl font-bold text-gray-900 dark:text-primary flex w-full px-4 mt-4 cursor-pointer">
+                  <span aria-label="Brand logo">
+                    <NavLink
+                      to={"/"}
+                      onClick={() => setOpen(false)}
+                      aria-label={`Navigate to home`}
+                    >
+                      MB
+                    </NavLink>
+                  </span>
                 </div>
                 <div className="px-4 sm:px-6 lg:px-8 py-3 h-16">
                   <MobileMenuBtn
@@ -112,13 +128,15 @@ function HomeLayout({ children }: { children: React.ReactNode }) {
                 </div>
               </div>
               <div className="h-full flex flex-col justify-between">
-                <ul className="flex flex-col">
+                <ul className="flex flex-col gap-2">
                   {navItems.map((item, index) => (
-                    <li key={index}>
+                    <li
+                      key={index}
+                      className="text-gray-900 dark:text-white hover:bg-gray-700 hover:text-white inline-block px-3 py-2 text-lg font-medium cursor-pointer"
+                    >
                       <NavLink
                         to={item.href}
                         onClick={() => setOpen(false)}
-                        className="text-gray-900 dark:text-white hover:bg-gray-700 hover:text-white px-3 py-2 text-lg font-medium"
                         aria-label={`Navigate to ${item.title}`}
                       >
                         {item.title}
