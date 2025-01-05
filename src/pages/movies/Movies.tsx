ec8c82a3726e0fbbb60b93b820625e4937d70c1f
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-01-05 20:57:19 +0530

    feat: move to top button(UI)

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -3,6 +3,7 @@ import FiltersConfig from "../../components/moviesPage/FilterControls";
 import MoviesList from "../../components/moviesPage/MoviesList";
 import Search from "../../components/moviesPage/Search";
 import FilterProvider from "../../context/FilterContext/FilterProvider";
+import { ArrowUp } from "lucide-react";
 
 function Movies() {
   return (
@@ -12,6 +13,9 @@ function Movies() {
         <Search />
         <FiltersConfig />
         <MoviesList />
+        <div className="dark:bg-primary bg-secondary text-primary dark:text-secondary p-4 m-4 bottom-0 right-0 rounded-full fixed cursor-pointer">
+          <ArrowUp />
+        </div>
       </div>
     </FilterProvider>
   );
