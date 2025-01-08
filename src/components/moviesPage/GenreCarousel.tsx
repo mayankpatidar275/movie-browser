7885caadbf8018f3c357327eac47577aa0588046
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2025-01-08 21:53:27 +0530

    fix: genre next prev button color conditionally

--- src/components/moviesPage/GenreCarousel.tsx
+++ src/components/moviesPage/GenreCarousel.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GenreItem, QueryParams } from "../../types";
 import GenreBtn from "./GenreBtn";
 
@@ -14,6 +14,7 @@ function GenreCarousel({
   toggleGenreSelection,
   state,
 }: GenreCarouselProps) {
+  const [sliderPosition, setSliderPosition] = useState("left");
   const sliderRef = useRef<HTMLDivElement>(null);
   function handlePrev() {
     const carouselWidth = sliderRef?.current?.clientWidth ?? 0;
@@ -25,21 +26,30 @@ function GenreCarousel({
   function handleNext() {
     const carouselWidth = sliderRef?.current?.clientWidth ?? 0;
     const scrollBy = sliderRef?.current?.scrollLeft ?? 0;
-    console.log("vall: ", sliderRef.current?.scrollLeft);
     if (sliderRef.current) {
       sliderRef.current.scrollLeft = scrollBy + carouselWidth;
     }
+  }
+
+  function updateSliderPosition() {
+    if (sliderRef?.current?.scrollLeft === 0) setSliderPosition("left");
+    else setSliderPosition("right");
   }
 
   return (
     <div className="genre-carousel overflow-hidden relative flex justify-center items-center">
       <button
         onClick={handlePrev}
-        className="pre-btn dark:text-primary text-secondary text-xl"
+        className={`pre-btn text-xl ${
+          sliderPosition === "left"
+            ? "text-gray-500"
+            : "dark:text-primary text-secondary "
+        }`}
       >
         <ChevronLeft />
       </button>
       <div
+        onScroll={updateSliderPosition}
         ref={sliderRef}
         className="w-full flex gap-2 p-4 overflow-x-scroll scroll-smooth no-scrollbar"
       >
@@ -69,7 +79,11 @@ function GenreCarousel({
       </div>
       <button
         onClick={handleNext}
-        className="pre-btn dark:text-primary text-secondary"
+        className={`pre-btn text-xl ${
+          sliderPosition === "right"
+            ? "text-gray-400"
+            : "dark:text-primary text-secondary "
+        }`}
       >
         <ChevronRight />
       </button>
